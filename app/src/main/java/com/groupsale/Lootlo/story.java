87fdbfc9fce9a0b3b10618932f3989e25a4ed790
santoshk66 <santosh.k66@gmail.com>
2021-04-21 17:14:15 +0530

    Ui done with story view in main code

---
+++ app/src/main/java/com/groupsale/Lootlo/story.java
@@ -0,0 +1,23 @@
+package com.groupsale.Lootlo;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class story extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+
+        // consider java code from
+        // status_deal java class only layout of story will combine with status deal tiktok demo screen
+    }
+    }
